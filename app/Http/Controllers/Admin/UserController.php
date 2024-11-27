0347ae04fec8b21422a60758bbd0ddcb576dd8d5
Lois Gonzales <loisjersong@gmail.com>
2024-11-27 11:03:35 +0800

    fix: change the admin user pagination from 1 to 10

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,7 +14,7 @@ use App\Models\Role;
 class UserController extends Controller
 {
     public function index() {
-        $users = User::paginate(1);
+        $users = User::paginate(10);
         return view('admin.users.index', ['users' => $users]);
     }
 
